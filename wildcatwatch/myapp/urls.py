19099da83c62fded97698b4af73048b799d32ba2
zydney <zydneybelia@gmail.com>
2025-10-31 02:40:10 +0800

    Ready for deployment

--- wildcatwatch/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views 
-
-urlpatterns = [
-    path("", views.home_page, name='home_page_root'),  # 👈 root goes to home_page now
-    path("register/", views.register, name='register'),
-    path("login/", views.login_view, name='login'),
-    path("home_page/", views.home_page, name='home_page'),
-]
